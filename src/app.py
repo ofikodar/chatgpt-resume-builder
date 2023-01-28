b8121984994f71958c1eb9d24badbe2e2b75d7e8
ofikodar <ofikodar@gmail.com>
2023-01-28 20:30:28 +0200

    add documntaions

--- src/app.py
+++ src/app.py
@@ -5,11 +5,19 @@ from src.chatbot.chatgpt import Chatgpt
 from src.utils import parse_pdf, export_html_resume
 
 
-def parse_and_improve_resume(args):
+def improve_resume(args):
+    """
+    This function takes the command line arguments, parses the input pdf resume using the parse_pdf function from the utils
+    module, passes the parsed data to the Chatgpt model's improve_resume method to generate an improved version of the
+    resume, and then exports the improved resume in html format using the export_html_resume function from the utils
+    module.
+    :param args: command line arguments containing the input and output file paths, and the data directory
+    :type args: argparse.Namespace
+    """
     input_path = os.path.join(args.data_dir, args.input_resume)
     parsed_resume = parse_pdf(input_path)
 
-    chatbot = Chatgpt()
+    chatbot = Chatgpt(args.config_path)
     new_resume_data = chatbot.improve_resume(parsed_resume)
 
     output_path = os.path.join(args.data_dir, args.output_file)
@@ -17,13 +25,22 @@ def parse_and_improve_resume(args):
 
 
 def main():
+    """
+    Main function to parse command line arguments and call the parse_and_improve_resume function.
+    """
     parser = argparse.ArgumentParser()
+
     parser.add_argument("--data_dir", type=str, default='../data', help="Directory containing input and output files")
+    parser.add_argument("--config_path", type=str, default='config.json', help="Path to the configuration file")
     parser.add_argument("--input_resume", type=str, default='example_input.pdf', help="Name of input resume file")
     parser.add_argument("--output_resume", type=str, default='new_resume.html', help="Name of output resume file")
+
     args = parser.parse_args()
 
-    parse_and_improve_resume(args)
+    try:
+        improve_resume(args)
+    except Exception as e:
+        print(f'An error occurred: {e}')
 
 
 if __name__ == '__main__':
