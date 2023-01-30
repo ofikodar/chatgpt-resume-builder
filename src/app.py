9333d9e1f8a0918f3c205c296aa06005c37db11b
ofikodar <ofikodar@gmail.com>
2023-01-31 00:07:52 +0200

    UI Draft

--- src/app.py
+++ src/app.py
@@ -23,55 +23,55 @@ DATA_FORMAT = {'name': 'John Doe', 'title': 'Data Scientist',
                           'Software development']}
 
 
-# Set the CSS style for the app
-def set_style():
-    with open('src/style.css') as f:
-        st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
-
-
-# Use the set_style function to add CSS to the app
-
-# Add header and input fields to the app
-
 def list_section(section_name, section_data):
     description_key = 'description'
 
     item_keys = list(section_data[0].keys())
     item_keys.remove(description_key)
-    for section_id, section_item in enumerate(section_data):
+    for item_id, section_item in enumerate(section_data):
         cols = st.columns(len(item_keys))
         for col, key in zip(cols, item_keys):
-            col.text_input(key, section_item[key], key=f'{section_name}_{section_id}_{key}')
+            col.text_input(key, section_item[key], key=f'{section_name}_{item_id}_{key}')
         st.text_area(description_key, section_item[description_key])
 
-    with st.container():
-        cols = st.columns(2)
-        cols[0].button("Auto Improve", key=f'{section_name}_improve_auto')
-        cols[1].text_input("Ask a recruiter", key=f'{section_name}_improve_manual')
+        recruiter_subsection(section_name, item_id)
+        st.markdown('***')
 
 
 def skills_section(section_name, skills_data):
-    for skill in skills_data:
-        st.info(skill)
+    num_columns = 3
+    print("2222")
+    for skills_row in range(0, len(skills_data), num_columns):
+        cols = st.columns([3,1] * num_columns)
+        skills_row_names = skills_data[skills_row: skills_row + num_columns]
+        for item_id, skill in enumerate(skills_row_names):
+            skill_id = skills_row  + item_id
+            print(item_id)
+            cols[item_id * 2].info(f'{skill}')
+            cols[item_id * 2 + 1].button('x', key=f'{section_name}_{skill_id}_remove_skill')
 
+    st.markdown('***')
+
+
+def recruiter_subsection(section_name, item_id=0):
     with st.container():
-        cols = st.columns(2)
-        cols[0].button("Auto Improve", key=f'{section_name}_improve_auto')
-        cols[1].text_input("Free Text", key=f'{section_name}_improve_manual')
+        cols = st.columns([3 ,10], gap='small')
+        cols[0].write('\n')
+        cols[0].write('\n')
+        cols[0].button("Auto Section Improve", key=f'{section_name}_{item_id}_improve_auto')
+        cols[1].text_input("", value="Send special request to the bot here...", key=f'{section_name}_{item_id}_improve_manual')
 
 
 def summary_section(section_name, summary_data):
     st.text_area('', summary_data, key=f'{section_name}')
-    with st.container():
-        cols = st.columns(2)
-        cols[0].button("Auto Improve", key=f'{section_name}_improve_auto')
-        cols[1].text_input("Ask a recruiter", key=f'{section_name}_improve_manual')
+    recruiter_subsection(section_name)
 
 
 def contact_info_section(section_name, info_data):
     for key, value in info_data.items():
         if value:
             st.text_input(key.title(), value)
+    st.markdown('***')
 
 
 def title(DATA_FORMAT):
@@ -80,9 +80,9 @@ def title(DATA_FORMAT):
 
 
 def body(DATA_FORMAT):
-    section_dict = {'contact_info': contact_info_section, 'summary': summary_section, 'work_experience': list_section,
-                    'education': list_section, 'skills': skills_section}
-
+    # section_dict = {'contact_info': contact_info_section, 'summary': summary_section, 'work_experience': list_section,
+    #                 'education': list_section, 'skills': skills_section}
+    section_dict = {'skills': skills_section}
     tabs_names = [key.replace('_', ' ').title() for key in section_dict.keys()]
     tabs = st.tabs(tabs_names)
     for tab, key in zip(tabs, section_dict):
@@ -98,8 +98,10 @@ def sidebar():
         st.button("Give Feedback")
         st.download_button('Download PDF', 'text_contents')
 
+
 def header():
     st.title("SolidCV - AI Resume Improver")
+
 
 def _main(DATA_FORMAT):
     header()
