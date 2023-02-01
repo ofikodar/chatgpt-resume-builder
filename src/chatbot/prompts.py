0a0c42a8e7aae78eab1197b43a19c7049638f269
ofikodar <ofikodar@gmail.com>
2023-02-01 23:44:45 +0200

    update prompts.py

--- src/chatbot/prompts.py
+++ src/chatbot/prompts.py
@@ -1,5 +1,7 @@
 from langchain import OpenAI, PromptTemplate
 
+prompt_placeholder = '[$$$]'
+
 data_format = {'name': '', 'title': '',
                'contactInfo': {'linkedin': '', 'github': '', 'email': '', 'address': '', 'phone': ''}, 'summary': '',
                'workExperience': [{'title': '', 'company': '', 'dates': '', 'description': ''},
@@ -8,20 +10,24 @@ data_format = {'name': '', 'title': '',
 
 recruiter_prompt = 'You are a recruiter and a professional resume builder.'
 command_prompt = 'Re-write the input as professionally as possible, adding vital and valuable information.'
-
+user_request_prompt = f'{prompt_placeholder}'
 output_format_prompts = dict()
 output_format_prompts['all'] = f'Return the output as dictionary in the next format {str(data_format)}.'
 output_format_prompts['section'] = f'Return the output as string.'
 
-input_prompt = 'Input: {input}'
+input_prompt = f'Input: {prompt_placeholder}'
 
 
-def get_template(command_type, spacial_request_prompt=''):
+def get_prompt(command_type, input_data ,user_request=''):
     valid_command_types = list(output_format_prompts)
     assert command_type in valid_command_types, f"Not valid command type, try {valid_command_types}"
-    if spacial_request_prompt:
-        spacial_request_prompt += '\n'
-    template = '\n'.join([recruiter_prompt, command_prompt, spacial_request_prompt, input_prompt , output_format_prompts[command_type]])
+    if user_request:
+        user_request += '\n'
+
+    template = '\n'.join(
+        [recruiter_prompt, command_prompt, user_request_prompt.replace(prompt_placeholder, user_request), input_prompt.replace(prompt_placeholder, input_data), output_format_prompts[command_type]])
     return template
 
-print(get_template('all','ASD'))
+command_type = 'all'
+template = get_prompt(command_type,'xx','sfdsf')
+print(template)
