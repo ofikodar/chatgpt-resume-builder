4b7b0c7d6aa18c078a508e1aab266dcd897f1d51
ofikodar <ofikodar@gmail.com>
2023-02-01 23:35:13 +0200

    update prompts.py

--- src/chatbot/prompts.py
+++ src/chatbot/prompts.py
@@ -1,12 +1,27 @@
-DATA_FORMAT = {'name': '', 'title': '',
-               'contact_info': {'linkedin': '', 'github': '', 'email': '', 'address': '', 'phone': ''}, 'summary': '',
-               'work_experience': [{'title': '', 'company': '', 'dates': '', 'description': ''},
-                                   {'title': '', 'company': '', 'dates': '', 'description': ''}, ],
+from langchain import OpenAI, PromptTemplate
+
+data_format = {'name': '', 'title': '',
+               'contactInfo': {'linkedin': '', 'github': '', 'email': '', 'address': '', 'phone': ''}, 'summary': '',
+               'workExperience': [{'title': '', 'company': '', 'dates': '', 'description': ''},
+                                  {'title': '', 'company': '', 'dates': '', 'description': ''}, ],
                'education': [{'degree': '', 'school': '', 'dates': '', 'description': ''}, ], 'skills': ['', '', '']}
 
-RESUME_PLACEHOLDER = '[$$$]'
-START_RECRUITER_PROMPT = 'You are a recruiter. Here a parsed resume, re-write it as professional as possible, add valuable and missing critical information.'
-DATA_FORMAT_PROMPT = F'Return a dictionary with the new data in this format: {str(DATA_FORMAT)}'
-RESUME_PROMPT = f'This is the Resume: {RESUME_PLACEHOLDER}\n'
-END_RECRUITER_PROMPT = '# professional it, add valuable and missing critical information, update it to maximize engament. Expand descriptions and skills.'
-PROMPT = '\n'.join([START_RECRUITER_PROMPT, DATA_FORMAT_PROMPT, RESUME_PROMPT, END_RECRUITER_PROMPT])
+recruiter_prompt = 'You are a recruiter and a professional resume builder.'
+command_prompt = 'Re-write the input as professionally as possible, adding vital and valuable information.'
+
+output_format_prompts = dict()
+output_format_prompts['all'] = f'Return the output as dictionary in the next format {str(data_format)}.'
+output_format_prompts['section'] = f'Return the output as string.'
+
+input_prompt = 'Input: {input}'
+
+
+def get_template(command_type, spacial_request_prompt=''):
+    valid_command_types = list(output_format_prompts)
+    assert command_type in valid_command_types, f"Not valid command type, try {valid_command_types}"
+    if spacial_request_prompt:
+        spacial_request_prompt += '\n'
+    template = '\n'.join([recruiter_prompt, command_prompt, spacial_request_prompt, input_prompt , output_format_prompts[command_type]])
+    return template
+
+print(get_template('all','ASD'))
