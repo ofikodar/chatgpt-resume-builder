e8cf6f19760580be8ffdf991910fbb54737b7a0d
ofikodar <ofikodar@gmail.com>
2023-02-04 20:18:09 +0200

    Update prompt

--- src/main.py
+++
@@ -1,9 +0,0 @@
-from revChatGPT.Official import Chatbot
-
-from src.chatbot.chatgpt import Chatgpt
-
-if __name__ == '__main__':
-    chatbot = Chatgpt('config.json')
-    y = chatbot.improve_section('My name is ofek i have an msc i lead NLP projects')
-    print(y)
-
